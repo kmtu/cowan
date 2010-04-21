a6f2e1962b458f0a04a54fd5ea689400a6a0e317
Kai-Ming Tu <tutu522@gmail.com>
2010-04-21 21:06:54 +0800

    initial commit

---
+++ src/cowan.f90
@@ -0,0 +1,8 @@
+!Coordinated Water Analysis (cowan)
+!This program is used to analyze the coordinated water molecules around an ion
+
+PROGRAM cowan
+  USE filehandle
+  IMPLICIT NONE
+
+END PROGRAM cowan
