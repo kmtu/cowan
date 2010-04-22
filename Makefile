159d3e26002a51c20fa43fd8d73172e1e2a82366
Kai-Ming Tu <tutu522@gmail.com>
2010-04-22 17:42:05 +0800

    Add Makefile, add argument reading ability.

---
+++ Makefile
@@ -0,0 +1,35 @@
+# Defining variables ===============================
+FC = gfortran
+MODFLAG = -J
+FCFLAGS = $(MODFLAG)$(MODDIR)
+
+SRCDIR = ./src/
+#OBJDIR = ./obj/
+MODDIR = ./mod/
+#LIBDIR = $(SRCDIR)lib/
+OUTDIR = ./out/
+
+PROGRAM = cowan
+
+vpath %.f90 $(SRCDIR)
+vpath %.o $(SRCDIR)
+vpath % $(OUTDIR)
+
+# Program ==================================
+all : $(PROGRAM)
+.PHONY : all 
+# ---------------------------
+cowan : cowan.o
+	$(FC) -o $(OUTDIR)$(@F) $(SRCDIR)$(<F)
+
+cowan.o : cowan.f90
+	$(FC) -o $(SRCDIR)$(@F) -c $< $(FCFLAGS)
+
+# Library ===================================
+#nrtype.o : nrtype.f90
+#	$(FC) -o $(LIBDIR)$(@F) -c $< $(FCFLAGS)
+#==================================================
+.PHONY: clean
+clean :
+	rm -f $(SRCDIR)*.o $(MODDIR)*.mod 
+	rm -f $(addprefix $(OUTDIR), $(PROGRAM) ) 
--- src/cowan.f90
+++ src/cowan.f90
@@ -2,7 +2,48 @@
 !This program is used to analyze the coordinated water molecules around an ion
 
 PROGRAM cowan
-  USE filehandle
   IMPLICIT NONE
-
+  INTEGER, PARAMETER :: REQUIRED_NUM_PAR = 3
+  INTEGER, PARAMETER :: input_fileid = 10
+  CHARACTER(LEN=128) :: input_filename
+  INTEGER :: count, i, stat, num_water
+  CHARACTER(LEN=128) :: command, usage, arg
+  !read the parameters from command line arguments
+  count = COMMAND_ARGUMENT_COUNT()
+  call GET_COMMAND_ARGUMENT(NUMBER=0, VALUE=command)
+  usage = "Usage: " // TRIM(ADJUSTL(command)) // " input_filename &
+          &-n <# of H2O>"
+  
+  if (count < REQUIRED_NUM_PAR) then
+     write(*,"(A,I1,A)") "At least ", REQUIRED_NUM_PAR, " arguments are needed."
+     write(*,*) usage
+     call EXIT(1)
+  end if
+  call GET_COMMAND_ARGUMENT(NUMBER=1, VALUE=input_filename, STATUS=stat)
+  if (stat /= 0) then
+     write(*,*) "Unable to get the input_filename."
+     write(*,*) usage        
+     call EXIT(1)
+  end if
+  
+  i = 2
+  do while (i <= count)
+     call GET_COMMAND_ARGUMENT(NUMBER=i, VALUE=arg, STATUS=stat)
+     if (stat /= 0) then
+        write(*,*) "Something wrong while reading arguments!"
+        write(*,*) usage
+        call EXIT(1)
+     end if
+     if (arg == '-n') then
+        i = i + 1
+        call GET_COMMAND_ARGUMENT(NUMBER=i, VALUE=arg, STATUS=stat)
+        read(arg,*) num_water
+     else
+        write(*,*) "Undefined argument: ", arg
+        write(*,*) usage
+        call EXIT(1)
+     end if
+     i = i + 1
+  end do
+  
 END PROGRAM cowan
