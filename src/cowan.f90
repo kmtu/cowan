80ad865cd112a8e69ac5f6738e4130d23a880422
Kai-Ming Tu <tutu522@gmail.com>
2010-04-22 14:37:03 +0800

    rearragne the git ignore pattern files

--- src/cowan.f90
+++
@@ -1,8 +0,0 @@
-!Coordinated Water Analysis (cowan)
-!This program is used to analyze the coordinated water molecules around an ion
-
-PROGRAM cowan
-  USE filehandle
-  IMPLICIT NONE
-
-END PROGRAM cowan
